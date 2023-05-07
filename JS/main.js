b66c794b7739d19cb0ebc0b288a47b633db83644
tema-baldi <artem26rus126@gmail.com>
2023-05-07 08:40:53 +0300

    fixed

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,12 @@
+const changeHeight = () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh',`${vh}px`);
+};
+changeHeight();
+
+window.addEventListener('resize', () => {
+  changeHeight();
+});
 
 const days = document.querySelector('#days');
 const hours = document.querySelector('#hours');
